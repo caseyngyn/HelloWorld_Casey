418385360ff51e1e39f039b179eca2aaa6b818bd
SCU Imaginarium (Ori) <>
2022-05-09 17:29:54 -0700

    ball

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -9,22 +9,41 @@ public class playerController : MonoBehaviour
     private float movementX;
     private float movementY;
     public float speed = 50;
+    public GameObject ball;
+    public GameObject playerCamera;
+    public float ballDistance = 2f;
+    public float ballThrowingForce = 5f;
+    private bool holdingBall = true;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        ball.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    void Update()
+    {
+        if (holdingBall) {
+            ball.transform.position = playerCamera.transform.position + playerCamera.transform.forward * ballDistance;
+            if(Input.GetMouseButtonDown(0))
+            {
+                holdingBall = false;
+                ball.GetComponent<Rigidbody>().useGravity = true;
+                ball.GetComponent<Rigidbody>().AddForce(playerCamera.transform.forward * ballThrowingForce);
+            }
+        }
     }
 
     // Update is called once per frame
+    /*
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement*speed);
     }
-
+    */
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
